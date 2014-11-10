885a67eb4d2f40623806258f954d66b9346c8bd2
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spam@nowhere>
2014-11-10 19:40:21 +0000

    Working again on GCC and Linux. In fact, works utterly perfectly, unlike on Windows :(

--- standalone_alltests_gcc.sh
+++ standalone_alltests_gcc.sh
@@ -2,4 +2,4 @@
 if [ ! -d asio ]; then
   git clone https://github.com/chriskohlhoff/asio.git
 fi
-g++ -o test_all -g -O3 -std=c++11 test/test_all.cpp detail/SpookyV2.cpp -Iinclude -Itest -DAFIO_STANDALONE=1 -Iasio/asio/include -DASIO_STANDALONE=1 -DBOOST_TEST_DYN_LINK=1 -lboost_filesystem -lboost_unit_test_framework -lboost_system -lpthread
+g++ -o test_all -g -O3 -std=c++11 test/test_all.cpp detail/SpookyV2.cpp -Iinclude -Itest -DAFIO_STANDALONE=1 -Iasio/asio/include -DASIO_STANDALONE=1  -DBOOST_AFIO_RUNNING_IN_CI=1 -lboost_filesystem -lboost_system -lpthread
