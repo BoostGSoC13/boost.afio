a6917c5d14ec49e5e6ffb7cebd82e522eaee23a5
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spam@nowhere>
2015-01-31 19:03:31 +0000

    More Android fixes.

--- standalone_alltests_gcc.sh
+++ standalone_alltests_gcc.sh
@@ -5,8 +5,11 @@ fi
 if [ "$CXX" != "${CXX#clang++}" ] && [ "$NODE_NAME" = "linux-gcc-clang" ]; then
   LIBATOMIC=-latomic
 fi
+if [ "$NODE_NAME" = "freebsd10-clang3.3" ]; then
+  LIBATOMIC=-L/usr/local/include
+fi
 if [ ! -d asio ]; then
-  git clone https://github.com/chriskohlhoff/asio.git
+  sh -c "git clone https://github.com/chriskohlhoff/asio.git"
 fi
 rm -rf test_all
 $CXX -o test_all -g -O2 -std=c++11 test/test_all.cpp detail/SpookyV2.cpp -Iinclude -Itest -DAFIO_STANDALONE=1 -Iasio/asio/include -DASIO_STANDALONE=1  -DBOOST_AFIO_RUNNING_IN_CI=1 -Wno-constexpr-not-const -Wno-c++1y-extensions -lboost_filesystem -lboost_system -lpthread $LIBATOMIC
