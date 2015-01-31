2b89223e31c6f1beaa24eb68c6816f9f47cc3eed
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spam@nowhere>
2015-01-31 14:45:06 +0000

    standalone alltests gcc now observes $CXX

--- standalone_alltests_gcc.sh
+++ standalone_alltests_gcc.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+if [ -z "$CXX" ]; then
+  CXX=g++
+fi
 if [ ! -d asio ]; then
   git clone https://github.com/chriskohlhoff/asio.git
 fi
 rm -rf test_all
-g++ -o test_all -g -O2 -std=c++11 test/test_all.cpp detail/SpookyV2.cpp -Iinclude -Itest -DAFIO_STANDALONE=1 -Iasio/asio/include -DASIO_STANDALONE=1  -DBOOST_AFIO_RUNNING_IN_CI=1 -lboost_filesystem -lboost_system -lpthread
+$CXX -o test_all -g -O2 -std=c++11 test/test_all.cpp detail/SpookyV2.cpp -Iinclude -Itest -DAFIO_STANDALONE=1 -Iasio/asio/include -DASIO_STANDALONE=1  -DBOOST_AFIO_RUNNING_IN_CI=1 -lboost_filesystem -lboost_system -lpthread
