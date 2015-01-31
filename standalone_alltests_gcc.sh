034d150e8b442d16ad151677379bf529c2c37c83
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spam@nowhere>
2015-01-31 19:43:02 +0000

    Fix warning standalone FreeBSD.

--- standalone_alltests_gcc.sh
+++ standalone_alltests_gcc.sh
@@ -12,4 +12,4 @@ if [ ! -d asio ]; then
   sh -c "git clone https://github.com/chriskohlhoff/asio.git"
 fi
 rm -rf test_all
-$CXX -o test_all -g -O2 -std=c++11 test/test_all.cpp detail/SpookyV2.cpp -Iinclude -Itest -DAFIO_STANDALONE=1 -Iasio/asio/include -DASIO_STANDALONE=1  -DBOOST_AFIO_RUNNING_IN_CI=1 -Wno-constexpr-not-const -Wno-c++1y-extensions -lboost_filesystem -lboost_system -lpthread $LIBATOMIC
+$CXX -o test_all -g -O2 -std=c++11 test/test_all.cpp detail/SpookyV2.cpp -Iinclude -Itest -DAFIO_STANDALONE=1 -Iasio/asio/include -DASIO_STANDALONE=1  -DBOOST_AFIO_RUNNING_IN_CI=1 -Wno-constexpr-not-const -Wno-c++1y-extensions -Wno-unused-value -lboost_filesystem -lboost_system -lpthread $LIBATOMIC
