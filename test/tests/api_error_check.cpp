9819118b360cf4a601594e8d626c6cad1af9f2dc
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spam@nowhere>
2015-02-05 18:20:00 +0000

    Revert "second trial"
    This reverts commit 00291681f350f05506471a252365233df0979742.

--- test/tests/api_error_check.cpp
+++ test/tests/api_error_check.cpp
@@ -2,49 +2,36 @@
 
 BOOST_AFIO_AUTO_TEST_CASE(api_error_check, "Tests that every API returns errors as it is supposed to", 20)
 {
-  using namespace BOOST_AFIO_V1_NAMESPACE;
-  auto dispatcher = make_async_file_io_dispatcher();
-#define BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(call, errcode)                                                                                                                                                                                                                                                                      \
-  try                                                                                                                                                                                                                                                                                                                          \
-  {                                                                                                                                                                                                                                                                                                                            \
-    call;                                                                                                                                                                                                                                                                                                                      \
-    BOOST_FAIL("Exception not thrown by " #call);                                                                                                                                                                                                                                                                              \
-  }                                                                                                                                                                                                                                                                                                                            \
-  catch(const system_error &e)                                                                                                                                                                                                                                                                                                 \
-  {                                                                                                                                                                                                                                                                                                                            \
-    BOOST_CHECK(e.code().value() == errcode);                                                                                                                                                                                                                                                                                  \
-  }                                                                                                                                                                                                                                                                                                                            \
-  catch(...)                                                                                                                                                                                                                                                                                                                   \
-  {                                                                                                                                                                                                                                                                                                                            \
-    BOOST_FAIL("Exception thrown by " #call " was not a system_error");                                                                                                                                                                                                                                                        \
-  }
+    using namespace BOOST_AFIO_V1_NAMESPACE;
+    auto dispatcher = make_async_file_io_dispatcher();
+#define BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(call, errcode) try { call; BOOST_FAIL("Exception not thrown by " #call); } catch(const system_error &e) { BOOST_CHECK(e.code().value()==errcode); } catch(...) { BOOST_FAIL("Exception thrown by " #call " was not a system_error"); }
 #ifdef WIN32
-#define BOOST_AFIO_FILE_NOT_FOUND_ERRCODE ERROR_FILE_NOT_FOUND
-#define BOOST_AFIO_BAD_FD ERROR_INVALID_HANDLE
+# define BOOST_AFIO_FILE_NOT_FOUND_ERRCODE ERROR_FILE_NOT_FOUND
+# define BOOST_AFIO_BAD_FD ERROR_INVALID_HANDLE
 #else
-#define BOOST_AFIO_FILE_NOT_FOUND_ERRCODE ENOENT
-#define BOOST_AFIO_BAD_FD EBADF
+# define BOOST_AFIO_FILE_NOT_FOUND_ERRCODE ENOENT
+# define BOOST_AFIO_BAD_FD EBADF
 #endif
 
-  // Create a bad handle
-  async_io_op op = dispatcher->file(async_path_op_req("testfile", file_flags::Create | file_flags::ReadWrite));
-  auto h = op.get();
-  dispatcher->rmfile(async_path_op_req(dispatcher->close(op), "testfile")).get();
-  char buffer[32];
+    // Create a bad handle
+	async_io_op op=dispatcher->file(async_path_op_req("testfile", file_flags::Create|file_flags::ReadWrite));
+	auto h=op.get();
+    dispatcher->rmfile(async_path_op_req(dispatcher->close(op), "testfile")).get();
+    char buffer[32];
 
-  BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->dir("should not exist this").get(), BOOST_AFIO_FILE_NOT_FOUND_ERRCODE);
-  BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->file("should not exist this").get(), BOOST_AFIO_FILE_NOT_FOUND_ERRCODE);
-  // No point checking sync(), he won't call if no bytes have been written
-  BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->zero(op, {{0, 0}}).get(), BOOST_AFIO_BAD_FD);
-  BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->read(make_async_data_op_req(op, buffer, 32)).get(), BOOST_AFIO_BAD_FD);
-  BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->write(make_async_data_op_req(op, buffer, 32)).get(), BOOST_AFIO_BAD_FD);
-  BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->truncate(op, 32).get(), BOOST_AFIO_BAD_FD);
-  BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->enumerate(op).first.get(), BOOST_AFIO_BAD_FD);
-  BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->enumerate(op).second.get(), BOOST_AFIO_BAD_FD);
-  BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->extents(op).first.get(), BOOST_AFIO_BAD_FD);
-  BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->extents(op).second.get(), BOOST_AFIO_BAD_FD);
-  BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->statfs(op, fs_metadata_flags::All).first.get(), BOOST_AFIO_BAD_FD);
-  BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->statfs(op, fs_metadata_flags::All).second.get(), BOOST_AFIO_BAD_FD);
+    BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->dir("should not exist this").get(), BOOST_AFIO_FILE_NOT_FOUND_ERRCODE);
+    BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->file("should not exist this").get(), BOOST_AFIO_FILE_NOT_FOUND_ERRCODE);
+    // No point checking sync(), he won't call if no bytes have been written
+    BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->zero(op, {{0, 0}}).get(), BOOST_AFIO_BAD_FD);
+    BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->read(make_async_data_op_req(op, buffer, 32)).get(), BOOST_AFIO_BAD_FD);
+    BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->write(make_async_data_op_req(op, buffer, 32)).get(), BOOST_AFIO_BAD_FD);
+    BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->truncate(op, 32).get(), BOOST_AFIO_BAD_FD);
+    BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->enumerate(op).first.get(), BOOST_AFIO_BAD_FD);
+    BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->enumerate(op).second.get(), BOOST_AFIO_BAD_FD);
+    BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->extents(op).first.get(), BOOST_AFIO_BAD_FD);
+    BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->extents(op).second.get(), BOOST_AFIO_BAD_FD);
+    BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->statfs(op, fs_metadata_flags::All).first.get(), BOOST_AFIO_BAD_FD);
+    BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE(dispatcher->statfs(op, fs_metadata_flags::All).second.get(), BOOST_AFIO_BAD_FD);
 
 #undef BOOST_AFIO_CHECK_SYSTEM_ERROR_CODE
 }
--- test/tests/async_io_works_1_autoflush_test.cpp
+++ test/tests/async_io_works_1_autoflush_test.cpp
@@ -3,11 +3,11 @@
 
 BOOST_AFIO_AUTO_TEST_CASE(async_io_works_1_autoflush, "Tests that the autoflush async i/o implementation works", 60)
 {
-  using namespace BOOST_AFIO_V1_NAMESPACE;
-  namespace asio = BOOST_AFIO_V1_NAMESPACE::asio;
+    using namespace BOOST_AFIO_V1_NAMESPACE;
+    namespace asio = BOOST_AFIO_V1_NAMESPACE::asio;
 #ifndef BOOST_AFIO_RUNNING_IN_CI
-  auto dispatcher = make_async_file_io_dispatcher(process_threadpool(), file_flags::SyncOnClose);
-  std::cout << "\n\n1000 file opens, writes 1 byte, closes, and deletes with autoflush i/o:\n";
-  _1000_open_write_close_deletes(dispatcher, 1);
+        auto dispatcher=make_async_file_io_dispatcher(process_threadpool(), file_flags::SyncOnClose);
+        std::cout << "\n\n1000 file opens, writes 1 byte, closes, and deletes with autoflush i/o:\n";
+        _1000_open_write_close_deletes(dispatcher, 1);
 #endif
 }
