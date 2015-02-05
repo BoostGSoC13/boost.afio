3076ad69eac1a1549ccf19079e690a9481f93b85
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spam@nowhere>
2015-02-05 18:23:07 +0000

    fourth trial

--- example/benchmark_unchained1.cpp
+++ example/benchmark_unchained1.cpp
@@ -4,40 +4,46 @@
     My Intel Core i7 3770K running     Linux x64:  1432810 closures/sec
 */
 
-static std::pair<bool, std::shared_ptr<boost::afio::async_io_handle>> callback(size_t, boost::afio::async_io_op op)
+static std::pair<bool, std::shared_ptr<boost::afio::async_io_handle>>
+callback(size_t, boost::afio::async_io_op op)
 {
 #if 0
     // Simulate an i/o op with a context switch
     Sleep(0);
 #endif
-    return std::make_pair(true, op.get());
+  return std::make_pair(true, op.get());
 };
 
 int main(void)
 {
-    using namespace boost::afio;
-    auto dispatcher=make_async_file_io_dispatcher();
-    typedef chrono::duration<double, ratio<1, 1>> secs_type;
-    auto begin=chrono::high_resolution_clock::now();
-    while(chrono::duration_cast<secs_type>(chrono::high_resolution_clock::now()-begin).count()<3);
-    
-    std::vector<async_io_op> preconditions;
-    std::vector<std::pair<async_op_flags, async_file_io_dispatcher_base::completion_t *>> callbacks(1,
-        std::make_pair(async_op_flags::none, callback));
+  using namespace boost::afio;
+  auto dispatcher = make_async_file_io_dispatcher();
+  typedef chrono::duration<double, ratio<1, 1>> secs_type;
+  auto begin = chrono::high_resolution_clock::now();
+  while(chrono::duration_cast<secs_type>(chrono::high_resolution_clock::now() -
+                                         begin).count() < 3)
+    ;
+
+  std::vector<async_io_op> preconditions;
+  std::vector<
+      std::pair<async_op_flags, async_file_io_dispatcher_base::completion_t *>>
+      callbacks(1, std::make_pair(async_op_flags::none, callback));
 #if 0
     std::cout << "Attach profiler now and hit Return" << std::endl;
     getchar();
 #endif
-    begin=chrono::high_resolution_clock::now();
+  begin = chrono::high_resolution_clock::now();
 #pragma omp parallel for
-    for(int n=0; n<5000000; n++)
-        dispatcher->completion(preconditions, callbacks);
-    while(dispatcher->wait_queue_depth())
-        this_thread::sleep_for(chrono::milliseconds(1));
-    auto end=chrono::high_resolution_clock::now();
-    auto diff=chrono::duration_cast<secs_type>(end-begin);
-    std::cout << "It took " << diff.count() << " secs to execute 5,000,000 closures which is " << (5000000/diff.count()) << " unchained closures/sec" << std::endl;
-    std::cout << "\nPress Return to exit ..." << std::endl;
-    getchar();
-    return 0;
+  for(int n = 0; n < 5000000; n++)
+    dispatcher->completion(preconditions, callbacks);
+  while(dispatcher->wait_queue_depth())
+    this_thread::sleep_for(chrono::milliseconds(1));
+  auto end = chrono::high_resolution_clock::now();
+  auto diff = chrono::duration_cast<secs_type>(end - begin);
+  std::cout << "It took " << diff.count()
+            << " secs to execute 5,000,000 closures which is "
+            << (5000000 / diff.count()) << " unchained closures/sec" << std::endl;
+  std::cout << "\nPress Return to exit ..." << std::endl;
+  getchar();
+  return 0;
 }
--- test/tests/async_io_pagesize.cpp
+++ test/tests/async_io_pagesize.cpp
@@ -2,18 +2,18 @@
 
 BOOST_AFIO_AUTO_TEST_CASE(async_io_pagesize, "Tests that the page size works, and other minor functions", 3)
 {
-    using namespace BOOST_AFIO_V1_NAMESPACE;
-    namespace asio = BOOST_AFIO_V1_NAMESPACE::asio;
-    try
-    {
-        auto dispatcher=make_async_file_io_dispatcher();
-        std::cout << "\n\nSystem page size is: " << dispatcher->page_size() << " bytes" << std::endl;
-        std::cout << "\n\nThread source use count is: " << dispatcher->threadsource().use_count() << std::endl;
-        BOOST_AFIO_CHECK_THROWS(dispatcher->op_from_scheduled_id(78));
-    }
-    catch(...)
-    {
-        std::cerr << "Exception thrown." << std::endl;
-        BOOST_CHECK(false);
-    }
+  using namespace BOOST_AFIO_V1_NAMESPACE;
+  namespace asio = BOOST_AFIO_V1_NAMESPACE::asio;
+  try
+  {
+    auto dispatcher = make_async_file_io_dispatcher();
+    std::cout << "\n\nSystem page size is: " << dispatcher->page_size() << " bytes" << std::endl;
+    std::cout << "\n\nThread source use count is: " << dispatcher->threadsource().use_count() << std::endl;
+    BOOST_AFIO_CHECK_THROWS(dispatcher->op_from_scheduled_id(78));
+  }
+  catch(...)
+  {
+    std::cerr << "Exception thrown." << std::endl;
+    BOOST_CHECK(false);
+  }
 }
