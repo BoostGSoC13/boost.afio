05f29d3fca00a6eadc9ecf1de5f7c544d5ec2be7
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spam@nowhere>
2015-02-05 18:17:41 +0000

    upd

--- clang-reformat.sh
+++ clang-reformat.sh
@@ -1,4 +1,7 @@
+find example -name *.cpp | xargs clang-format-3.6 -i
 clang-format-3.6 -i include/boost/afio/afio.hpp
 #clang-format-3.6 -i include/boost/afio/config.hpp
 find include/boost/afio/detail -name *.hpp | xargs clang-format-3.6 -i
 find include/boost/afio/detail -name *.ipp | xargs clang-format-3.6 -i
+find test -name *.hpp | xargs clang-format-3.6 -i
+find test -name *.cpp | xargs clang-format-3.6 -i
