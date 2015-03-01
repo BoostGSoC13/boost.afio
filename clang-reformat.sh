9c3a0b78bc2569089c93f63a36ca88fb432379fc
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spam@nowhere>
2015-03-01 14:14:46 +0000

    Fixed missing quotes.

--- clang-reformat.sh
+++ clang-reformat.sh
@@ -8,10 +8,10 @@ find test -name "*.hpp" | xargs clang-format-3.6 -i
 find test -name "*.cpp" | xargs clang-format-3.6 -i
 
 PWD=$(pwd)
-find example -name "*.cpp" | xargs $PWD/scripts/IndentCmacros.py
+find example -name "*.cpp" | xargs "$PWD/scripts/IndentCmacros.py"
 scripts/IndentCmacros.py include/boost/afio/afio.hpp
 scripts/IndentCmacros.py include/boost/afio/config.hpp
-find include/boost/afio/detail -name "*.hpp" | xargs $PWD/scripts/IndentCmacros.py
-find include/boost/afio/detail -name "*.ipp" | xargs $PWD/scripts/IndentCmacros.py
-find test -name "*.hpp" | xargs $PWD/scripts/IndentCmacros.py
-find test -name "*.cpp" | xargs $PWD/scripts/IndentCmacros.py
+find include/boost/afio/detail -name "*.hpp" | xargs "$PWD/scripts/IndentCmacros.py"
+find include/boost/afio/detail -name "*.ipp" | xargs "$PWD/scripts/IndentCmacros.py"
+find test -name "*.hpp" | xargs "$PWD/scripts/IndentCmacros.py"
+find test -name "*.cpp" | xargs "$PWD/scripts/IndentCmacros.py"
