d85c90da658d7d79eeaf6ad0f542780c4d0b0e0f
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spam@nowhere>
2015-03-07 23:41:14 +0000

    Use zero termination for clang reformat.

--- clang-reformat.sh
+++ clang-reformat.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -x
-find example -name "*.cpp" | xargs clang-format-3.6 -i
+find example -print0 -name "*.cpp" | xargs -0 clang-format-3.6 -i
 clang-format-3.6 -i include/boost/afio/afio.hpp
 #clang-format-3.6 -i include/boost/afio/config.hpp
-find include/boost/afio/detail -name "*.hpp" | xargs clang-format-3.6 -i
-find include/boost/afio/detail -name "*.ipp" | xargs clang-format-3.6 -i
-find test -name "*.hpp" | xargs clang-format-3.6 -i
-find test -name "*.cpp" | xargs clang-format-3.6 -i
+find include/boost/afio/detail -print0 -name "*.hpp" | xargs -0 clang-format-3.6 -i
+find include/boost/afio/detail -print0 -name "*.ipp" | xargs -0 clang-format-3.6 -i
+find test -print0 -name "*.hpp" | xargs -0 clang-format-3.6 -i
+find test -print0 -name "*.cpp" | xargs -0 clang-format-3.6 -i
 
 PWD=$(pwd)
-find example -name "*.cpp" | xargs "$PWD/scripts/IndentCmacros.py"
+find example -print0 -name "*.cpp" | xargs -0 "$PWD/scripts/IndentCmacros.py"
 scripts/IndentCmacros.py include/boost/afio/afio.hpp
 scripts/IndentCmacros.py include/boost/afio/config.hpp
-find include/boost/afio/detail -name "*.hpp" | xargs "$PWD/scripts/IndentCmacros.py"
-find include/boost/afio/detail -name "*.ipp" | xargs "$PWD/scripts/IndentCmacros.py"
-find test -name "*.hpp" | xargs "$PWD/scripts/IndentCmacros.py"
-find test -name "*.cpp" | xargs "$PWD/scripts/IndentCmacros.py"
+find include/boost/afio/detail -print0 -name "*.hpp" | xargs -0 "$PWD/scripts/IndentCmacros.py"
+find include/boost/afio/detail -print0 -name "*.ipp" | xargs -0 "$PWD/scripts/IndentCmacros.py"
+find test -print0 -name "*.hpp" | xargs -0 "$PWD/scripts/IndentCmacros.py"
+find test -print0 -name "*.cpp" | xargs -0 "$PWD/scripts/IndentCmacros.py"
