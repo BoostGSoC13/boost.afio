8f4f62e33f7eb68ad37382de065237d44e36c3f9
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spam@nowhere>
2015-03-08 04:10:28 +0000

    Try again.

--- clang-reformat.sh
+++ clang-reformat.sh
@@ -1,17 +1,17 @@
 #!/bin/bash -x
-find example -print0 -name "*.cpp" | xargs -0 clang-format-3.6 -i
+find example -name "*.cpp" | xargs -d '\n' clang-format-3.6 -i
 clang-format-3.6 -i include/boost/afio/afio.hpp
 #clang-format-3.6 -i include/boost/afio/config.hpp
-find include/boost/afio/detail -print0 -name "*.hpp" | xargs -0 clang-format-3.6 -i
-find include/boost/afio/detail -print0 -name "*.ipp" | xargs -0 clang-format-3.6 -i
-find test -print0 -name "*.hpp" | xargs -0 clang-format-3.6 -i
-find test -print0 -name "*.cpp" | xargs -0 clang-format-3.6 -i
+find include/boost/afio/detail -name "*.hpp" | xargs -d '\n' clang-format-3.6 -i
+find include/boost/afio/detail -name "*.ipp" | xargs -d '\n' clang-format-3.6 -i
+find test -name "*.hpp" | xargs -d '\n' clang-format-3.6 -i
+find test -name "*.cpp" | xargs -d '\n' clang-format-3.6 -i
 
 PWD=$(pwd)
-find example -print0 -name "*.cpp" | xargs -0 "$PWD/scripts/IndentCmacros.py"
+find example -name "*.cpp" | xargs -d '\n' "$PWD/scripts/IndentCmacros.py"
 scripts/IndentCmacros.py include/boost/afio/afio.hpp
 scripts/IndentCmacros.py include/boost/afio/config.hpp
-find include/boost/afio/detail -print0 -name "*.hpp" | xargs -0 "$PWD/scripts/IndentCmacros.py"
-find include/boost/afio/detail -print0 -name "*.ipp" | xargs -0 "$PWD/scripts/IndentCmacros.py"
-find test -print0 -name "*.hpp" | xargs -0 "$PWD/scripts/IndentCmacros.py"
-find test -print0 -name "*.cpp" | xargs -0 "$PWD/scripts/IndentCmacros.py"
+find include/boost/afio/detail -name "*.hpp" | xargs -d '\n' "$PWD/scripts/IndentCmacros.py"
+find include/boost/afio/detail -name "*.ipp" | xargs -d '\n' "$PWD/scripts/IndentCmacros.py"
+find test -name "*.hpp" | xargs -d '\n' "$PWD/scripts/IndentCmacros.py"
+find test -name "*.cpp" | xargs -d '\n' "$PWD/scripts/IndentCmacros.py"
