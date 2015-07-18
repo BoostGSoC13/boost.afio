9e30a03a4c962e9081cc8fea74cda49aea8ecf16
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nowhere.com>
2015-07-18 16:29:30 +0100

    Fix broken clang-reformat

--- clang-reformat.sh
+++ clang-reformat.sh
@@ -8,10 +8,10 @@ find test -name "*.hpp" -exec clang-format-3.6 -i {} ';'
 find test -name "*.cpp" -exec clang-format-3.6 -i {} ';'
 
 PWD=$(pwd)
-find example -name "*.cpp" -exec "$PWD/scripts/IndentCmacros.py" {} ';'
-scripts/IndentCmacros.py include/boost/afio/afio.hpp
-scripts/IndentCmacros.py include/boost/afio/config.hpp
-find include/boost/afio/detail -name "*.hpp" -exec "$PWD/scripts/IndentCmacros.py" {} ';'
-find include/boost/afio/detail -name "*.ipp" -exec "$PWD/scripts/IndentCmacros.py" {} ';'
-find test -name "*.hpp" -exec "$PWD/scripts/IndentCmacros.py" {} ';'
-find test -name "*.cpp" -exec "$PWD/scripts/IndentCmacros.py" {} ';'
+find example -name "*.cpp" -exec "$PWD/include/boost/afio/bindlib/scripts/IndentCmacros.py" {} ';'
+include/boost/afio/bindlib/scripts/IndentCmacros.py include/boost/afio/afio.hpp
+include/boost/afio/bindlib/scripts/IndentCmacros.py include/boost/afio/config.hpp
+find include/boost/afio/detail -name "*.hpp" -exec "$PWD/include/boost/afio/bindlib/scripts/IndentCmacros.py" {} ';'
+find include/boost/afio/detail -name "*.ipp" -exec "$PWD/include/boost/afio/bindlib/scripts/IndentCmacros.py" {} ';'
+find test -name "*.hpp" -exec "$PWD/include/boost/afio/bindlib/scripts/IndentCmacros.py" {} ';'
+find test -name "*.cpp" -exec "$PWD/include/boost/afio/bindlib/scripts/IndentCmacros.py" {} ';'
