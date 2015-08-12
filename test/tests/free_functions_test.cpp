798b64d3640ed936717e2cb2d82004fd3dc0df98
Niall Douglas (s [underscore] sourceforge {at} nedprod [dot] com) <spamtrap@nowhere.com>
2015-08-12 12:32:00 +0100

    Fix unit test failure on Windows.

--- test/tests/free_functions_test.cpp
+++ test/tests/free_functions_test.cpp
@@ -9,21 +9,21 @@ BOOST_AFIO_AUTO_TEST_CASE(free_functions_work, "Tests that the free functions wo
   current_dispatcher_guard h(make_dispatcher().get());
 
   {
-    auto diropened = async_dir("testdir", file_flags::create);
+    auto diropened = async_dir("testdir", file_flags::create | file_flags::write);
     auto fileopened = async_file(diropened, "testfile", file_flags::create | file_flags::write);
     auto filedeleted = async_rmfile(fileopened);
     auto dirdeleted = async_rmdir(depends(filedeleted, diropened));
     dirdeleted.get();
   }
   {
-    auto diropened = dir("testdir", file_flags::create);
+    auto diropened = dir("testdir", file_flags::create | file_flags::write);
     auto fileopened = file(diropened, "testfile", file_flags::create | file_flags::write);
     auto filedeleted = rmfile(fileopened);
     auto dirdeleted = rmdir(diropened);
   }
   {
     error_code ec;
-    auto diropened = dir(ec, "testdir", file_flags::create);
+    auto diropened = dir(ec, "testdir", file_flags::create | file_flags::write);
     auto fileopened = file(ec, diropened, "testfile", file_flags::create | file_flags::write);
     auto filedeleted = rmfile(ec, fileopened);
     auto dirdeleted = rmdir(ec, diropened);
